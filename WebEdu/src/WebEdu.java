6de19b5b0d2be8622c1d46573880b367b67a7ad1
Borisja <boki_@live.nl>
2017-09-29 12:25:28 +0200

    test

---
+++ WebEdu/src/WebEdu.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Boris Janjic
+ *
+ */
+public class WebEdu {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
