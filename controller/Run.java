5bf89f2450e4f7ac840420ca76399e91821c72b2
nach7vos <joshwow91@gmail.com>
2017-10-02 10:14:38 +0200

    login v0.1

--- controller/Run.java
+++ controller/Run.java
@@ -1,9 +1,18 @@
 package controller;
+
+import model.gebruikerModel;
+
 public class Run {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
+		loginController lc = new loginController();
+		gebruikerModel main_gebruiker = lc.login_assignment("lmao@lmao.com", "password");
+		if(main_gebruiker == null){
+			System.out.println("Login failed.");
+		} else {
+			System.out.println("Login success, assigning user id... " + main_gebruiker.getGebruikerId());
+		}
 	}
 
 }
--- model/gebruikerModel.java
+++ model/gebruikerModel.java
@@ -1,5 +1,74 @@
 package model;
 
 public class gebruikerModel {
-
+	private int gebruikerId;
+	
+	private String gebruikerVoornaam;
+	private String gebruikerAchternaam;
+	private String gebruikerWachtwoord;
+	private String gebruikerEmail;
+	
+	private int gebruikerTotaalUren;
+	private int gebruikerSleutel;
+	private int gebruikerRol;
+	
+	public gebruikerModel(int id, String vnaam, String anaam, String wachtwoord, String email, int totaaluren, int sleutel, int rol){
+		this.gebruikerId = id;
+		this.gebruikerVoornaam = vnaam;
+		this.gebruikerAchternaam = anaam;
+		this.gebruikerWachtwoord = wachtwoord;
+		this.gebruikerEmail = email;
+		this.gebruikerTotaalUren = totaaluren;
+		this.gebruikerSleutel = sleutel;
+		this.gebruikerRol = rol;
+	}
+	
+	public int getGebruikerId() {
+		return gebruikerId;
+	}
+	public void setGebruikerId(int gebruikerId) {
+		this.gebruikerId = gebruikerId;
+	}
+	public String getGebruikerVoornaam() {
+		return gebruikerVoornaam;
+	}
+	public void setGebruikerVoornaam(String gebruikerVoornaam) {
+		this.gebruikerVoornaam = gebruikerVoornaam;
+	}
+	public String getGebruikerAchternaam() {
+		return gebruikerAchternaam;
+	}
+	public void setGebruikerAchternaam(String gebruikerAchternaam) {
+		this.gebruikerAchternaam = gebruikerAchternaam;
+	}
+	public String getGebruikerWachtwoord() {
+		return gebruikerWachtwoord;
+	}
+	public void setGebruikerWachtwoord(String gebruikerWachtwoord) {
+		this.gebruikerWachtwoord = gebruikerWachtwoord;
+	}
+	public String getGebruikerEmail() {
+		return gebruikerEmail;
+	}
+	public void setGebruikerEmail(String gebruikerEmail) {
+		this.gebruikerEmail = gebruikerEmail;
+	}
+	public int getGebruikerTotaalUren() {
+		return gebruikerTotaalUren;
+	}
+	public void setGebruikerTotaalUren(int gebruikerTotaalUren) {
+		this.gebruikerTotaalUren = gebruikerTotaalUren;
+	}
+	public int getGebruikerSleutel() {
+		return gebruikerSleutel;
+	}
+	public void setGebruikerSleutel(int gebruikerSleutel) {
+		this.gebruikerSleutel = gebruikerSleutel;
+	}
+	public int getGebruikerRol() {
+		return gebruikerRol;
+	}
+	public void setGebruikerRol(int gebruikerRol) {
+		this.gebruikerRol = gebruikerRol;
+	}
 }
