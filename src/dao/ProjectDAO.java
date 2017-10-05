eadb6b568835a65df15163c46f074c59cdedc141
Admin <Admin@DESKTOP-5E3ODDE>
2017-10-05 13:20:50 +0200

    Vorige Update

---
+++ src/dao/ProjectDAO.java
@@ -0,0 +1,51 @@
+package dao;
+
+import java.sql.*;
+
+
+import javafx.collections.ObservableList;
+import model.ProjectModel;
+
+public class ProjectDAO {
+	
+	ObservableList<ProjectModel> arrProjecten;
+	
+	
+	
+	public ProjectDAO(){
+		System.out.println("test");
+		ConnectDAO connect = new ConnectDAO();
+		try {
+			Statement statement = connect.connectToDB().createStatement();
+			ResultSet resultSet = statement.executeQuery("select * from project");
+			ResultSetMetaData metaData = resultSet.getMetaData();
+			System.out.println("test");
+			int numberOfColumns = metaData.getColumnCount();
+			
+			while (resultSet.next())
+			{ 
+				for(int kolom = 1; kolom <= numberOfColumns; kolom++)
+					System.out.println(resultSet.getString(kolom)+"\t\t\t");
+				System.out.println("\n=====");
+			}
+			
+			statement.close();
+			connect.connectToDB().close();
+			
+			
+		} catch (SQLException e) {
+		
+			e.printStackTrace();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+
+	
+		
+	
+	
+	
+}
---
+++ src/model/ProjectModel.java
@@ -0,0 +1,30 @@
+package model;
+
+public class ProjectModel {
+	
+	String project_id;
+	String project_isdeleted;
+	
+	public ProjectModel(String project_id, String project_isdeleted){
+		this.project_id = project_id;
+		this.project_isdeleted = project_isdeleted;
+	}
+
+	public String getProject_id() {
+		return project_id;
+	}
+
+	public void setProject_id(String project_id) {
+		this.project_id = project_id;
+	}
+
+	public String getProject_isdeleted() {
+		return project_isdeleted;
+	}
+
+	public void setProject_isdeleted(String project_isdeleted) {
+		this.project_isdeleted = project_isdeleted;
+	}
+	
+
+}
