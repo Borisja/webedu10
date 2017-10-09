7faea789f5312c7fb8fdacbace11bc6e491b966f
Admin <Admin@145.101.78.144>
2017-10-09 13:19:47 +0200

    test

---
+++ src/view/GebruikersController.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+
+public class GebruikersController {
+	
+
+	@FXML ImageView imageView;
+
+	public void Toonproject(){
+		System.out.println("test");
+		
+		
+	}
+	
+}
