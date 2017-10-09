06351fccb158b7fb859007523beb8a5f88ccd918
Jeroen Zandvliet <Lissandratheicedervish@gmail.com>
2017-10-09 09:40:13 +0200

    View placed someplace else

---
+++ src/view/DeclareEntryView.java
@@ -0,0 +1,16 @@
+package view;
+
+import javafx.scene.control.Label;
+
+public class DeclareEntryView 
+{
+	Label startTime;
+	Label endTime;
+	Label project;
+	Label sprint;
+	Label userStory;
+	Label notitie;
+	
+	
+	
+}
