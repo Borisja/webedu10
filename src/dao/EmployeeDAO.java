650529e4bfe0ef719e2efdfa9618e938148a3d5f
nach7vos <joshwow91@gmail.com>
2017-10-12 13:30:46 +0200

    new agenda view

--- src/dao/EmployeeDAO.java
+++ src/dao/EmployeeDAO.java
@@ -51,7 +51,7 @@ public class EmployeeDAO {
 	 */
 	public ArrayList<EntryModel> entry_list(int e_id){
 		ArrayList<EntryModel> entry_alist = new ArrayList<EntryModel>();
-		String employee_entry_sql = "SELECT entry_version_starttime, entry_version_endtime, entry_version_creationtime "
+		String employee_entry_sql = "SELECT entry_version_starttime, entry_version_endtime, entry_version_creationtime, entry_version_description "
 				+ "FROM entry_version, entry "
 				+ "WHERE entry_employee_fk = ?";
 		try {
@@ -61,6 +61,7 @@ public class EmployeeDAO {
 			ResultSet entry_set = entries_statement.executeQuery();
 			while(entry_set.next()) {
 				EntryModel dummy = new EntryModel();
+				dummy.setEntryDescription(entry_set.getString("entry_version_description"));
 				dummy.setEntryStartTime(entry_set.getString("entry_version_starttime"));
 				dummy.setEntryEndTime(entry_set.getString("entry_version_endtime"));
 				dummy.setEntryDate(entry_set.getString("entry_version_creationtime"));
