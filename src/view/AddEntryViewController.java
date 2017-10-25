170e5b1a673358a86d07dbd1665cdfe05c0416cd
nach7vos <joshwow91@gmail.com>
2017-10-25 14:07:15 +0200

    working combobox?

--- src/view/AddEntryViewController.java
+++ src/view/AddEntryViewController.java
@@ -1,14 +1,17 @@
 package view;
 
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.ResourceBundle;
 
 import dao.ProjectDAO;
 import dao.SprintDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListCell;
@@ -19,6 +22,7 @@ import javafx.util.StringConverter;
 import model.EntryModel;
 import model.ProjectModel;
 import model.SprintModel;
+import model.UserStoryModel;
 
 /**
  * Deze klasse is bestemd voor invullen entries.
@@ -26,44 +30,118 @@ import model.SprintModel;
  *
  */
 
-public class AddEntryViewController {
+public class AddEntryViewController implements Initializable {
 	
-	@FXML ComboBox projectCombo;
+	@FXML ComboBox<ProjectModel> projectCombo;
 	@FXML ComboBox userStorysCombo;
-	@FXML ComboBox sprintCombo;
+	@FXML ComboBox<SprintModel> sprintCombo;
 	@FXML Button btnAddEntry;
 	
-	public void fillSprints(int nr)
+	/**
+	 * Fill the sprint box when project box onaction is called
+	 * @param p_id - project id from the selected model
+	 */
+	public void fillSprintsBox(int p_id)
 	{
-		ArrayList<SprintModel> pList = new SprintDAO().sprintsProjects(nr);
-		ObservableList<SprintModel> data;
-		ObservableList<String> dataNames;
-		data = FXCollections.observableArrayList();
-		dataNames = FXCollections.observableArrayList();
-		pList.forEach(e -> data.add(e));
-		pList.forEach(e -> dataNames.add(e.getSprintName()));
-		this.sprintCombo.setItems(dataNames);
-	}
-	
-	public void fillProjectsBox()
-	{
-		refresh();
+		ArrayList<SprintModel> sprint_list = new SprintDAO().sprintsProjects(p_id);
 		
-		if(projectCombo.getSelectionModel().getSelectedItem().equals(1))
-		{
-			fillSprints(1);
-		}						
+		ObservableList<SprintModel> data;
+		
+		data = FXCollections.observableArrayList();
+		
+		sprint_list.forEach(e -> data.add(e));
+
+		/**
+		 * Change what you see in the combobox, to the omschrijving rather then the object address.
+		 * Must study this to understand. works for now.
+		 */
+		Callback<ListView<SprintModel>, ListCell<SprintModel>> factory = lv -> new ListCell<SprintModel>() {
+		    @Override
+		    protected void updateItem(SprintModel item, boolean empty) {
+		        super.updateItem(item, empty);
+		        setText(empty ? "" :item.getSprintName());
+		    }
+
+		};
+		
+		this.sprintCombo.setItems(data);
+		this.sprintCombo.setCellFactory(factory);
+		this.sprintCombo.setButtonCell(factory.call(null));
 	}
 	
-	public void refresh()
-	{
-		ArrayList<ProjectModel> pList = new ProjectDAO().project_list();
-		ObservableList<ProjectModel> data;
-		ObservableList<Integer> dataNames;
+	/**
+	 * Fill the user story box when project box onaction is called
+	 * @param p_id - project id from the selected model
+	 */	
+	public void fillUserStoriesBox(int p_id)
+	{	
+//		ArrayList<UserStoryModel> pList = new ProjectDAO().project_list();
+		
+		ObservableList<UserStoryModel> data;
+		
 		data = FXCollections.observableArrayList();
-		dataNames = FXCollections.observableArrayList();
-		pList.forEach(e -> data.add(e));
-		pList.forEach(e -> dataNames.add(e.getProjectId()));
-		this.projectCombo.setItems(dataNames);
+		
+//		pList.forEach(e -> data.add(e));
+
+		/**
+		 * Change what you see in the combobox, to the omschrijving rather then the object address.
+		 * Must study this to understand. works for now.
+		 */
+		Callback<ListView<UserStoryModel>, ListCell<UserStoryModel>> factory = lv -> new ListCell<UserStoryModel>() {
+		    @Override
+		    protected void updateItem(UserStoryModel item, boolean empty) {
+		        super.updateItem(item, empty);
+		        setText(empty ? "" :item.getUserStoryName());
+		    }
+
+		};
+		
+		this.userStorysCombo.setItems(data);
+		this.userStorysCombo.setCellFactory(factory);
+		this.userStorysCombo.setButtonCell(factory.call(null));
+		
 	}
+	
+	/**
+	 * Fill the project box, when item is selected load the rest.
+	 */
+	public void fillProjectsBox()
+	{	
+		ArrayList<ProjectModel> pList = new ProjectDAO().project_list();
+		
+		ObservableList<ProjectModel> data;
+		
+		data = FXCollections.observableArrayList();
+		
+		pList.forEach(e -> data.add(e));
+
+		/**
+		 * Change what you see in the combobox, to the omschrijving rather then the object address.
+		 * Must study this to understand. works for now.
+		 */
+		Callback<ListView<ProjectModel>, ListCell<ProjectModel>> factory = lv -> new ListCell<ProjectModel>() {
+		    @Override
+		    protected void updateItem(ProjectModel item, boolean empty) {
+		        super.updateItem(item, empty);
+		        setText(empty ? "" :item.getProjectName());
+		    }
+
+		};
+		
+		this.projectCombo.setItems(data);
+		this.projectCombo.setCellFactory(factory);
+		this.projectCombo.setButtonCell(factory.call(null));
+		
+		this.projectCombo.setOnAction(e-> {
+			this.fillSprintsBox(projectCombo.getSelectionModel().getSelectedItem().getProjectId());
+		});
+	}
+
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		this.fillProjectsBox();
+	}
+
 }
