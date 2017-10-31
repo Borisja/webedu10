dfe1f9703eda7736cb7c54f66481fdbb4986edbe
Admin <Admin@145.101.74.103>
2017-10-31 15:49:26 +0100

    SprintDAO opvragen sprint project DAO query gemaakt

--- src/dao/ProjectDAO.java
+++ src/dao/ProjectDAO.java
@@ -89,11 +89,14 @@ public class ProjectDAO {
 	 * Deze methode geeft een overzicht van alle projecten die de employee aan deelneemt
 	 * @param customerModel
 	 * @return
-	 * @author rezanaser
+	 * @author Fardin Samandar
 	 */
 	public ArrayList<ProjectModel> project_list_employee(int employeeId){
 		ArrayList<ProjectModel> proj_list = new ArrayList<ProjectModel>();
-		String project_list_sql = "SELECT * FROM project_version";
+		String project_list_sql = "SELECT project_version_id, project_version_name, project_version_description FROM project_version"
++ " inner join project_employee on(project_employee_project_fk=project_version_project_fk)"
++ " inner join employee on(employee_id=project_employee_employee_fk)"
++ " where employee_id = ?";
 		try {
 			PreparedStatement project_statement = connect.connectToDB().prepareStatement(project_list_sql);
 			project_statement.setInt(1, employeeId);
--- src/view/sprintView/SprintController.java
+++ src/view/sprintView/SprintController.java
@@ -3,12 +3,30 @@ package view.sprintView;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import dao.SprintDAO;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.Pane;
+import model.EmployeeModel;
+import model.SprintModel;
 
 public class SprintController  implements Initializable {
+	
 	@FXML Pane pane;
+	@FXML TableView<SprintModel> sprintTabelView;
+	@FXML TableColumn<SprintModel, Integer> sId;
+	@FXML TableColumn<SprintModel, String> sprintName;
+	@FXML TableColumn<SprintModel, String> sprintStartDate;
+	@FXML TableColumn<SprintModel, String> sprintEndDate;
+	private SprintDAO sprintDAO = new SprintDAO();
+	private EmployeeModel emModel;
+	private ObservableList<SprintModel>sprintData = FXCollections.observableArrayList();
+	
 	
 	public void openSprintmenu(){
 		pane.setVisible(true);
@@ -21,7 +39,16 @@ public class SprintController  implements Initializable {
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		
+	//sId.setCellValueFactory(new PropertyValueFactory<SprintModel, Integer>("projectID"));	
+	sprintName.setCellValueFactory(new PropertyValueFactory<SprintModel, String>("sprintname"));		
+	sprintStartDate.setCellValueFactory(new PropertyValueFactory<SprintModel, String>("SprintStart"));		
+	sprintEndDate.setCellValueFactory(new PropertyValueFactory<SprintModel, String>("SprintEind"));
+	sprintData.addAll(sprintDAO.sprint_list());
+	sprintTabelView.setItems(sprintData);
+	}
+
+	public void setCurrentUser(EmployeeModel em) {
+		this.emModel = em;
 		
 	}
 	
